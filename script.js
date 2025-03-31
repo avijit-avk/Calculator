f1495909feb2591990f29e7912ec9440102dd989
avijit-avk <avijit01998.ak@gmail.com>
2025-03-31 23:13:30 +0530

    Updated calculator project

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('display');
+let resetDisplay = false; 
+
+function appendToDisplay(value) {
+    if (resetDisplay) {
+        display.value = ''; 
+        resetDisplay = false; 
+    }
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+    resetDisplay = false; 
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+        resetDisplay = true; 
+    } catch (error) {
+        display.value = 'Error';
+        resetDisplay = true; 
+    }
+}
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
